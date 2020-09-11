3f74429744b419c33e9f6aad200a271f55e3c770
sanyaele <samuelanyaele@gmail.com>
2020-09-11 11:06:19 +0100

    local jquery

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -148,10 +148,7 @@
 	@endif
 
 	<!-- jQuery 3.1.1 -->
-	<!--
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
--->
-	<script src="{{ asset('js/jquery-3.3.1.min.js') }}"></script>
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.15.1/moment.min.js"></script>
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datetimepicker/4.17.47/js/bootstrap-datetimepicker.min.js"></script>
