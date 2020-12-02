cf743c9b0cdf65b4de7422b2891702daab9694f1
sanyaele <samuelanyaele@gmail.com>
2020-12-02 01:42:26 +0100

    CSV completing

--- app/Http/Controllers/FaqBotController.php
+++ app/Http/Controllers/FaqBotController.php
@@ -11,31 +11,19 @@ class FaqBotController extends Controller
 	//
 	public function exportCsv(Request $request)
 	{
-		$fileName = 'faqs.csv';
+		$fileName = Storage::path('public/csv/faqs.csv');
 		$faqs = Faq::all();
 
-		$headers = array(
-			"Content-type"        => "text/csv",
-			"Content-Disposition" => "attachment; filename=" . $fileName, // <- name of file
-			"Pragma"              => "no-cache",
-			"Cache-Control"       => "must-revalidate, post-check=0, pre-check=0",
-			"Expires"             => "0",
-		);
 
-		$callback = function () use ($faqs) {
-			$file = fopen('php://output', 'w'); //<-here. name of file is written in headers
-			//fputcsv($file, ['Question', 'Answer']);
-			foreach ($faqs as $res) {
-				fputcsv($file, [$res->question, $res->answer]);
-			}
-			fclose($file);
-		};
+		$file = fopen($fileName, 'w'); //<-here. name of file is written in headers
+		foreach ($faqs as $res) {
+			fputcsv($file, [$res->question, $res->answer]);
+		}
+		fclose($file);
 
-		Storage::put('csv/faq.csv', $callback);
+		$this->uploadCsv(Storage::get('public/csv/faqs.csv'));
 
-		$this->uploadCsv(Storage::get('csv/faq.csv'));
-
-		return response()->stream($callback, 200, $headers);
+		return view('stored');
 	}
 
 	public function uploadCsv($file)
---
+++ resources/views/stored.blade.php
@@ -0,0 +1,19 @@
+@extends('layouts.app2')
+
+@section('content')
+<div class="container">
+	<div class="row justify-content-center">
+		<div class="col-md-8">
+			<div class="card">
+				<div class="card-header">Update Cloud CSV</div>
+
+				<div class="card-body">
+					CSV successfully updated.
+					<br>
+					Thank you!
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+@endsection
